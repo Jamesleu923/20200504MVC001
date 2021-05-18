254933faaa6134125947780f87844a7fa8248738
James Lu <jamesleu923@gmail.com>
2021-05-18 10:39:17 +0800

    MVC Model
    MVC Model

---
+++ MySQLException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _20210504MVC001
+{
+    public class MySQLException:Exception
+    {
+        public override string ToString()
+        {
+            return " 存檔錯誤";
+        }
+    }
+}
